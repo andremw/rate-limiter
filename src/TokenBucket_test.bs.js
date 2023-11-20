0840043efa9bdcf0fd152403ea3d7bc9aea0ca35
Andre <mazoni.andre@gmail.com>
2023-11-20 19:31:55 +1030

    passing capacity to makeBucket

--- src/TokenBucket_test.bs.js
+++ src/TokenBucket_test.bs.js
@@ -3,19 +3,23 @@
 
 var Jest = require("@glennsl/rescript-jest/src/jest.bs.js");
 
-function makeBucket(param, _request) {
-  return {
-          TAG: /* Error */1,
-          _0: undefined
-        };
+function makeBucket(capacity, _request) {
+  if (capacity !== 0) {
+    return {
+            TAG: /* Ok */0,
+            _0: undefined
+          };
+  } else {
+    return {
+            TAG: /* Error */1,
+            _0: undefined
+          };
+  }
 }
 
 Jest.describe("Token Bucket Algorithm", (function (param) {
         Jest.test("When a request arrives and the bucket is empty, the request is declined", (function (param) {
-                return Jest.Expect.toEqual(Jest.Expect.expect({
-                                TAG: /* Error */1,
-                                _0: undefined
-                              }), {
+                return Jest.Expect.toEqual(Jest.Expect.expect(makeBucket(0, "some.ip")), {
                             TAG: /* Error */1,
                             _0: undefined
                           });
--- src/TokenBucket_test.res
+++ src/TokenBucket_test.res
@@ -4,14 +4,19 @@ open Expect
 type request = string // for now this is going to be a string
 type requestResult = Result.t<unit, unit> // for now, just a simple result with no data inside
 type handleRequest = (request) => requestResult
-type makeBucket = () => handleRequest
+type makeBucket = (~capacity: int) => handleRequest
 
-let makeBucket: makeBucket = () => (_request) => Error()
+let makeBucket: makeBucket = (~capacity) => (_request) => {
+  switch capacity {
+  | 0 => Error()
+  | _ => Ok()
+  }
+}
 
 describe("Token Bucket Algorithm", () => {
   test("When a request arrives and the bucket is empty, the request is declined", () => {
     let request = "some.ip"
-    let handleRequest = makeBucket()
+    let handleRequest = makeBucket(~capacity=0)
     request->handleRequest->expect->toEqual(Error())
   })
 })
